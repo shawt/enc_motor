867e9764c69520b2f5b954e9a42012de1ea951f0
Trevor Shaw <shawt@d-e.org>
2019-05-24 10:54:53 -0400

    updates 1

--- enc_motor.cpp
+++ enc_motor.cpp
@@ -9,40 +9,74 @@
 #include "Arduino.h"
 #include "enc_motor.h"
 
-enc_motor::enc_motor(int ratio)
+enc_motor::enc_motor(int ratio, int encApin, int encBpin)
 {
     _ratio = ratio;
+    //based on pinout for Sparkfun Wireless motor shield
     _aPwm = 3;
     _aIn1 = 4;
     _aIn2 = 5;
     _bPwm = 6;
     _bIn1 = 7;
     _bIn2 = 8;
-   
+    _kp =1;
+    _partialTurn = (_ratio * 8) / 16; //8 pole magnet will generate 8 pin-change interrupt calls per encoder rotation. This means that th$
+    
+    pinMode (encApin, INPUT_PULLUP);
+    pinMode (encBpin, INPUT_PULLUP);
+    enc_motor::pciSetup(encApin);
+    enc_motor::pciSetup(encBpin);
+    
+    
+    pinMode(_aPwm, OUTPUT);
+    pinMode(_aIn1, OUTPUT);
+    pinMode(_aIn2, OUTPUT);
+    pinMode(_bPwm, OUTPUT);
+    pinMode(_bIn1, OUTPUT);
+    pinMode(_bIn2, OUTPUT);
+    
+    
     
 }
 
 void enc_motor::enc_tickA()
 {
-    int partialTurn = (_ratio * 8)/16; //8 pole magnet on the encoder measures 16ths of turns. Note that with a 1:1 ratio, the most precise we can be is 1/8th of a turn. Consider changing this.
+    //handle pin change interrupt for D0-7
     _ticksA++;
-    if (_ticksA % partialTurn == 0)
+    _errTicksA++;
+    if (_ticksA % _partialTurn == 0)
     {
-        _turnsA += .0625;
+        _ticksA = 0;
+        _turnsA += .0625; //one sixteenth of a turn
     }
 }
+
 
 void enc_motor::enc_tickB()
 {
-    int partialTurn = (_ratio * 8)/16; //8 pole magnet on the encoder measures 16ths of turns
+    //handle pin change interrupt for D8-13
     _ticksB++;
-    if (_ticksB % partialTurn == 0)
+    _errTicksB++;
+    
+    if (_ticksB % _partialTurn == 0)
     {
-        _turnsB += .0625;
+        _ticksB = 0;
+        _turnsB += .0625; //one sixteenth of a turn
+        
     }
 }
 
+
+void enc_motor::pciSetup(byte pin)
+{
+    //enable the given pin as an interrupt
+    *digitalPinToPCMSK(pin) |= bit (digitalPinToPCMSKbit(pin));  // enable pin
+    PCIFR  |= bit (digitalPinToPCICRbit(pin)); // clear any outstanding interrupt
+    PCICR  |= bit (digitalPinToPCICRbit(pin)); // enable interrupt for the group
+}
+
 void enc_motor::enc_fwd(double reqTurns, int speed)
+
 {
     analogWrite(_aPwm, speed);
     digitalWrite(_aIn1, HIGH);
@@ -50,9 +84,24 @@ void enc_motor::enc_fwd(double reqTurns, int speed)
     analogWrite(_bPwm, speed);
     digitalWrite(_bIn1, HIGH);
     digitalWrite(_bIn2, LOW);
+    int speedA = speed;
+    int speedB = speed;
+    int error = 0;
+    _ticksA = 0;
+    _ticksB = 0;
+    _errTicksA = 0;
+    _errTicksB = 0;
     
-    while (_turnsA < reqTurns) //this function is for both motors, but we only need one encoder, so use A
+    
+    
+    while (_turnsA < reqTurns && _turnsB < reqTurns) //this function is for both motors
     {
+        delay(100);
+        error = (_ticksA - _ticksB) / _kp;
+        speedB += error;
+        analogWrite(6,speedB);
+        //_errTicksA = 0;
+        //_errTicksB = 0;
         
     }
     analogWrite(_aPwm, 0);
@@ -62,5 +111,294 @@ void enc_motor::enc_fwd(double reqTurns, int speed)
     digitalWrite(_bIn1, HIGH);
     digitalWrite(_bIn2, HIGH);
     _turnsA = 0.0;
+    _turnsB = 0.0;
     
+}
+
+
+void enc_motor::enc_bak(double reqTurns, int speed)
+
+{
+    analogWrite(_aPwm, speed);
+    digitalWrite(_aIn1, LOW);
+    digitalWrite(_aIn2, HIGH);
+    analogWrite(_bPwm, speed);
+    digitalWrite(_bIn1, LOW);
+    digitalWrite(_bIn2, HIGH);
+    int speedA = speed;
+    int speedB = speed;
+    int error = 0;
+    _ticksA = 0;
+    _ticksB = 0;
+    
+    
+    while (_turnsA < reqTurns && _turnsB < reqTurns) //this function is for both motors
+    {
+        delay(100);
+        error = (_ticksA - _ticksB) / _kp;
+        speedB += error;
+        analogWrite(6,speedB);
+        //_errTicksA = 0;
+        //_errTicksB = 0;
+        
+        
+    }
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsA = 0.0;
+    _turnsB = 0.0;
+    
+    
+}
+
+void enc_motor::enc_spinRt(double reqTurns, int speed)
+{
+    analogWrite(_aPwm, speed);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, LOW);
+    analogWrite(_bPwm, speed);
+    digitalWrite(_bIn1, LOW);
+    digitalWrite(_bIn2, HIGH);
+    int speedA = speed;
+    int speedB = speed;
+    int error = 0;
+    _ticksA = 0;
+    _ticksB = 0;
+    
+ 
+    while (_turnsA < reqTurns && _turnsB < reqTurns) //this function is for both motors
+    {
+        
+        error = (_ticksA - _ticksB) / _kp;
+        speedB += error;
+        analogWrite(6,speedB);
+        delay(100);
+    }
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsA = 0.0;
+    _turnsB = 0.0;
+}
+void enc_motor::enc_spinLt(double reqTurns, int speed)
+{
+    analogWrite(_aPwm, speed);
+    digitalWrite(_aIn1, LOW);
+    digitalWrite(_aIn2, HIGH);
+    analogWrite(_bPwm, speed);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, LOW);
+    int speedA = speed;
+    int speedB = speed;
+    int error = 0;
+    _ticksA = 0;
+    _ticksB = 0;
+    
+
+    while (_turnsA < reqTurns && _turnsB < reqTurns) //this function is for both motors
+    {
+        
+        error = (_ticksA - _ticksB) / _kp;
+        speedB += error;
+        analogWrite(6,speedB);
+        delay(100);
+    }
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsA = 0.0;
+    _turnsB = 0.0;
+}
+
+void enc_motor::enc_stop()
+{
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsA = 0.0;
+    _turnsB = 0.0;
+}
+
+void enc_motor::enc_fwdA(double reqTurns, int speed)
+
+{
+    analogWrite(_aPwm, speed);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, LOW);
+    
+    
+    
+    while (_turnsA < reqTurns)
+    {
+        
+    }
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    _turnsA = 0.0;
+    
+}
+
+void enc_motor::enc_bakA(double reqTurns, int speed)
+
+{
+    analogWrite(_aPwm, speed);
+    digitalWrite(_aIn1, LOW);
+    digitalWrite(_aIn2, HIGH);
+   
+ 
+  
+    while (_turnsA < reqTurns)
+    {
+       
+    }
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    _turnsA = 0.0;
+    
+}
+
+void enc_motor::enc_spinRtA(double reqTurns, int speed)
+{
+    analogWrite(_aPwm, speed);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, LOW);
+    
+    while (_turnsA < reqTurns )
+    {
+        
+     
+    }
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    _turnsA = 0.0;
+}
+void enc_motor::enc_spinLtA(double reqTurns, int speed)
+{
+    analogWrite(_aPwm, speed);
+    digitalWrite(_aIn1, LOW);
+    digitalWrite(_aIn2, HIGH);
+    
+   
+    while (_turnsA < reqTurns )
+    {
+      
+    }
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    
+    _turnsA = 0.0;
+  
+}
+
+void enc_motor::enc_stopA()
+{
+    analogWrite(_aPwm, 0);
+    digitalWrite(_aIn1, HIGH);
+    digitalWrite(_aIn2, HIGH);
+    _turnsA = 0.0;
+
+}
+
+void enc_motor::enc_fwdB(double reqTurns, int speed)
+
+{
+    analogWrite(_bPwm, speed);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, LOW);
+
+    
+   
+    while ( _turnsB < reqTurns)
+    {
+        
+       
+    }
+    
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsB = 0.0;
+    
+}
+
+void enc_motor::enc_bakB(double reqTurns, int speed)
+
+{
+    analogWrite(_bPwm, speed);
+    digitalWrite(_bIn1, LOW);
+    digitalWrite(_bIn2, HIGH);
+
+    
+   
+    while (_turnsB < reqTurns) //this function is for both motors
+    {
+      
+    }
+
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+
+    _turnsB = 0.0;
+    
+}
+
+void enc_motor::enc_spinRtB(double reqTurns, int speed)
+{
+
+    analogWrite(_bPwm, speed);
+    digitalWrite(_bIn1, LOW);
+    digitalWrite(_bIn2, HIGH);
+
+    while (_turnsB < reqTurns)
+    {
+       
+    }
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsB = 0.0;
+}
+void enc_motor::enc_spinLtB(double reqTurns, int speed)
+{
+    
+    analogWrite(_bPwm, speed);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, LOW);
+
+    while (_turnsB < reqTurns)
+    {
+        
+        
+    }
+    
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsB = 0.0;
+}
+
+void enc_motor::enc_stopB()
+{
+    analogWrite(_bPwm, 0);
+    digitalWrite(_bIn1, HIGH);
+    digitalWrite(_bIn2, HIGH);
+    _turnsB = 0.0;
 }
--- enc_motor.h
+++ enc_motor.h
@@ -14,13 +14,29 @@
 class enc_motor
 {
 public:
-    enc_motor(int ratio);
+    enc_motor(int ratio, int encApin, int encBpin);
+    void pciSetup(byte pin);
     void enc_tickA();
     void enc_tickB();
+    
     void enc_fwd(double turns, int speed);
     void enc_bak(double turns, int speed);
     void enc_spinRt(double turns, int speed);
     void enc_spinLt(double turns, int speed);
+    void enc_stop();
+    
+    void enc_fwdA(double turns, int speed);
+    void enc_bakA(double turns, int speed);
+    void enc_spinRtA(double turns, int speed);
+    void enc_spinLtA(double turns, int speed);
+    void enc_stopA();
+    
+    void enc_fwdB(double turns, int speed);
+    void enc_bakB(double turns, int speed);
+    void enc_spinRtB(double turns, int speed);
+    void enc_spinLtB(double turns, int speed);
+    void enc_stopB();
+
     
 private:
     int _ratio;
@@ -30,10 +46,12 @@ private:
     int _bIn1;
     int _bIn2;
     int _bPwm;
-    volatile int _ticksA;
+    int _partialTurn;
+    int _kp //constant divider for errors
+    volatile long _ticksA;
     volatile float _turnsA;
-    volatile int _ticksB;
-    volatile int _turnsB;
+    volatile long _ticksB;
+    volatile float _turnsB;
     
 };
 
